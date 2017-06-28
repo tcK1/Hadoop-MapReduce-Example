3d5bc089aaa6490566782ca6e8264e3459ca7621
Lucas Delboni <lucas_cd460@hotmail.com>
2017-06-27 22:48:53 -0300

    iniciado a validacao das entradas no mapper

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Scanner;
 
 import org.apache.hadoop.conf.Configuration;
@@ -17,15 +19,58 @@ import org.apache.tools.ant.taskdefs.MacroDef.Text;
 
 public class Main {
 
-	private final SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+	static String startDate;
+
+	static String endDate;
 
 	// mapper
 	public static class WeatherMapper extends Mapper<LongWritable, Text, Text, DoubleWritable> {
+
+		private final SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
+		private boolean isValidDate(Date actual, String startDate, String endDate) throws ParseException {
+			Date start = dateFormat.parse(startDate);
+			Date end = dateFormat.parse(endDate);
+
+			if (actual.after(start) && actual.before(end)) {
+				return true;
+			}
+			return false;
+		}
 
 		@Override
 		protected void map(LongWritable key, Text value,
 				Mapper<LongWritable, Text, Text, DoubleWritable>.Context context)
 				throws IOException, InterruptedException {
+
+			String line = value.toString();
+
+			// o arquivo começa com "STN"... Ignorar primeira linha
+			if (line.startsWith("S")) {
+				return;
+			}
+
+			int year = Integer.parseInt(line.substring(14, 18));
+			int month = Integer.parseInt(line.substring(18, 20));
+			int day = Integer.parseInt(line.substring(20, 21));
+			System.out.println("data: " + day + "/" + month + "/" + year);
+
+			// verifica se a data lida é para ser analisada
+			try {
+				Calendar calendar = Calendar.getInstance();
+				calendar.clear();
+				calendar.set(year, month, day);
+				Date actual = calendar.getTime();
+				if (isValidDate(actual, startDate, endDate)) {
+					return;
+				}
+			} catch (ParseException e) {
+				System.err.println("Data no formato invalido ");
+				e.printStackTrace();
+			}
+
+			// se tudo certo ate agora, começa as contas
+
 		}
 	}
 
@@ -75,9 +120,9 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("insisra a data de inicio no formato DD/MM/AAAA");
-		String startDate = sc.nextLine();
+		startDate = sc.nextLine();
 		System.out.println("insira a data final no formato DD/MM/AAAA");
-		String endDate = sc.nextLine();
+		endDate = sc.nextLine();
 
 		System.out.println("Como deseja que os dados sejam agrupados?");
 		System.out.println("Ano-Mes-Dia: T"); // Tudo
